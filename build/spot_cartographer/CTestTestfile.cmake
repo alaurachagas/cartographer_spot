c694140f7c7807f51bbbe28c3200ea8372fcd0db
Ana Laura Soethe Chagas <ana.soethe.chagas@wzl-iqs.rwth-aachen.de>
2025-08-11 11:18:03 +0200

    reorganize package

--- build/spot_cartographer/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/hiwi/devel_ana/carto_ws/src/spot_cartographer
-# Build directory: /home/hiwi/devel_ana/carto_ws/build/spot_cartographer
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
