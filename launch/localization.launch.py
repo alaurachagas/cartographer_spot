b97711900935a700bd1cd8a38c263b911423e2ea
Ana Laura Soethe Chagas <ana.soethe.chagas@wzl-iqs.rwth-aachen.de>
2025-08-26 09:57:56 +0200

    Added config and launch for Localization only

---
+++ launch/localization.launch.py
@@ -0,0 +1,37 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+from launch.substitutions import PathJoinSubstitution
+
+def generate_launch_description():
+    cfg_dir = PathJoinSubstitution([FindPackageShare('spot_cartographer'), 'config'])
+    lua_basename = 'real_spot_localization.lua'
+
+    carto = Node(
+        package='cartographer_ros',
+        executable='cartographer_node',
+        name='cartographer_node',
+        arguments=[
+            '-configuration_directory', cfg_dir,
+            '-configuration_basename', lua_basename,
+            '-load_state_filename', '/map/spot_halle.pbstream',
+            '-load_frozen_state', 'true'
+        ],
+        remappings=[
+            ('points2', '/velodyne_points'),
+            ('odom',    '/odometry'),       # your Spot odom topic
+        ],
+        parameters=[{'use_sim_time': False}],
+        output='screen',
+    )
+
+    # cartographer_occupancy_grid_node = Node(
+    #     package = 'cartographer_ros',
+    #     executable = 'cartographer_occupancy_grid_node',
+    #     parameters = [
+    #         {'resolution': 0.05},
+    #         {'use_sim_time': False}
+    #         ],
+    #     )
+
+    return LaunchDescription([carto, cartographer_occupancy_grid_node])
